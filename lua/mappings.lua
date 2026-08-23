637133b468ff713d2e388c464ce24cb8339e88ba
Gsus <gsus04@gmail.com>
2026-08-23 02:38:17 +0200

    KeyBinding: Añadimos ESPACIO + Q para cerrar la pestaña que tenemos abierta.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -10,6 +10,11 @@ map("i", "jk", "<ESC>")
 -- Guardar archivo con Espacio + w en modo normal
 map("n", "<leader>w", "<cmd>w<CR>", { desc = "Guardar archivo" })
 
+-- Cerrar ÚNICAMENTE la pestaña actual sin romper las ventanas/splits
+map("n", "<leader>q", function()
+  require("nvchad.tabufline").close_buffer()
+end, { desc = "Cerrar pestaña actual" })
+
 -- DESACTIVAR terminales por defecto de NvChad en <leader>v y <leader>h
 vim.keymap.del("n", "<leader>v")
 vim.keymap.del("n", "<leader>h")
